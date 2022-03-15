5c373edac7ad61c2ffad1d879defd4c9ea1936b6
gxmari007 <392041299@qq.com>
2022-03-15 22:55:57 +0800

    feat: divider 组件支持 type、dashed 属性

--- src/runtime/components/divider/divider.stories.ts
+++ src/runtime/components/divider/divider.stories.ts
@@ -1,19 +1,59 @@
 import type { Meta, Story } from '@storybook/vue3'
 
-import Divider from './divider.vue'
+import NButton from '../button/button.vue'
+import NDivider from './divider.vue'
 
 const meta: Meta = {
   title: 'design-system/Divider',
-  component: Divider,
+  component: NDivider,
+  decorators: [() => ({ template: '<div class="w-420px text-base"><story /></div>' })],
+  argTypes: {
+    type: {
+      description: '水平还是垂直类型',
+      control: { type: 'select' },
+      options: ['horizontal', 'vertical'],
+    },
+    dashed: {
+      description: '是否虚线',
+    },
+  },
 }
 
-const Template: Story = (args) => ({
-  components: { NDivider: Divider },
+export const Horizontal: Story = (args) => ({
+  components: { NDivider },
   setup: () => ({ args }),
-  template: '<n-divider v-bind="args">{{ args.default }}</n-divider>',
+  template: `
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.</p>
+    <n-divider v-bind="args">{{ args.default }}</n-divider>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.</p>
+  `,
 })
+Horizontal.args = {
+  type: 'horizontal',
+}
 
-export const Horizontal = Template.bind({})
-Horizontal.args = {}
+export const Vertical: Story = (args) => ({
+  components: {
+    NDivider,
+    NButton,
+  },
+  setup: () => ({ args }),
+  template: `
+    <span>Text</span>
+    <n-divider v-bind="args">{{ args.default }}</n-divider>
+    <span>Text</span>
+    <n-divider v-bind="args">{{ args.default }}</n-divider>
+    <span>Text</span>
+  `,
+})
+Vertical.args = {
+  type: 'vertical',
+}
+
+export const Dashed = Horizontal.bind({})
+Dashed.args = {
+  ...Horizontal.args,
+  dashed: true,
+}
 
 export default meta
--- src/runtime/components/divider/divider.vue
+++ src/runtime/components/divider/divider.vue
@@ -10,17 +10,32 @@ import { PropType, computed } from 'vue'
 const props = defineProps({
   /** 水平还是垂直类型 */
   type: { type: String as PropType<'horizontal' | 'vertical'>, default: 'horizontal' },
+  /** 是否虚线 */
+  dashed: { type: Boolean, default: false },
 })
 
 const classes = computed(() => ({
   'n-divider': true,
   'n-divider-horizontal': props.type === 'horizontal',
   'n-divider-vertical': props.type === 'vertical',
+  'n-divider-dashed': props.dashed,
 }))
 </script>
 
 <style>
 .n-divider {
-  @apply p-0 text-content text-base border-t;
+  @apply box-border p-0 m-0 text-content text-base tabular-nums leading-base list-none border-t border-solid border-[rgba(0,0,0,.06)];
+}
+
+.n-divider-horizontal {
+  @apply flex clear-both w-full min-w-full my-lg;
+}
+
+.n-divider-vertical {
+  @apply relative top-[-0.06em] inline-block h-[0.9em] mx-xs align-middle border-t-0 border-l;
+}
+
+.n-divider-dashed {
+  @apply bg-transparent border-[#0000000f] border-dashed;
 }
 </style>
