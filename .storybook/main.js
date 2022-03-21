832d75c90e13c997633e9f92a974d7d9485a4d73
James Gao <392041299@qq.com>
2022-03-21 10:04:11 +0800

    chore: storybook 切换到 webpack 模式，删除 storybook 案例代码

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,14 @@
-const windicss = require('vite-plugin-windicss').default
+const WindiCSS = require('windicss-webpack-plugin')
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/vue3',
-  core: {
-    builder: 'storybook-builder-vite',
-  },
-  viteFinal(config) {
-    config.plugins.push(windicss())
+  // core: {
+  //   builder: 'storybook-builder-vite',
+  // },
+  webpackFinal(config) {
+    config.plugins.push(new WindiCSS())
     return config
   },
 }
