c9bd6953da01ecef81303d71b3a03b17687c5205
gxmari007 <392041299@qq.com>
2022-05-30 23:51:39 +0800

    chore: (wip) `Space`

--- src/runtime/components/space/space.stories.ts
+++ src/runtime/components/space/space.stories.ts
@@ -4,22 +4,40 @@ import NButton from '../button/button.vue'
 import NSpace from './space.vue'
 
 const meta: Meta = {
-  title: '设计系统/布局/Space 布局',
+  title: '设计系统/布局/间距 Space',
   component: NSpace,
 }
 
-export const Template: Story = (args) => ({
+export const Default: Story = (args) => ({
   components: {
     NSpace,
     NButton,
   },
   setup: () => ({ args }),
   template: `
-    <n-space>
+    <n-space v-bind="args">
       Space
       <n-button>Button</n-button>
-    </space>
+      <n-button type="primary">Primary</n-button>
+      <n-button type="link">Link</n-button>
+    </n-space>
   `,
 })
+
+export const Size: Story = (args) => ({
+  components: {
+    NSpace,
+    NButton,
+  },
+  setup: () => ({ args }),
+  template: `
+    <n-space v-bind="args">
+      <n-button v-for="n in 5">Button</n-button>
+    </n-space>
+  `,
+})
+Size.args = {
+  size: 'default',
+}
 
 export default meta
--- src/runtime/components/space/space.vue
+++ src/runtime/components/space/space.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="n-space">
+  <div class="n-space" :style="style">
     <div v-for="(child, index) in childs" :key="index">
       <component :is="child" />
     </div>
@@ -7,19 +7,59 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, useSlots } from 'vue'
+import type { PropType, StyleValue } from 'vue'
+import { ref, computed, useSlots, onUpdated } from 'vue'
 
 import { filterChildren } from '../utils/utils'
 
-defineProps({
+type Size = 'small' | 'default' | 'large' | number
+
+const props = defineProps({
   /** 间距大小 */
-  size: { type: [String, Number, Array], default: 'small' },
+  size: {
+    type: [String, Number, Array] as PropType<Size | Size[]>,
+    default: 'small',
+  },
+  /** 对齐方式 */
+  align: { type: String as PropType<'start' | 'end' | 'center' | 'baseline'>, default: 'center' },
 })
 
 // Slot
 const slots = useSlots()
+console.log(slots.default?.())
 const genChilds = () => filterChildren(slots.default?.())
 const childs = ref(genChilds())
+
+onUpdated(() => {
+  childs.value = genChilds()
+})
+
+// Size
+const sizeMap = {
+  small: '8px',
+  default: '16px',
+  large: '24px',
+}
+const gap = computed(() => {
+  let value = ''
+
+  if (Array.isArray(props.size)) {
+    const [y, x] = props.size
+    const yValue = typeof y === 'number' ? `${y}px` : sizeMap[y]
+    const xValue = typeof x === 'number' ? `${x}px` : sizeMap[x]
+
+    value = `${yValue} ${xValue}`
+  } else {
+    value = typeof props.size === 'number' ? `${props.size}px` : sizeMap[props.size]
+  }
+
+  return value
+})
+
+const style = computed<StyleValue>(() => ({
+  gap: gap.value,
+  alignItems: props.align,
+}))
 </script>
 
 <style>
