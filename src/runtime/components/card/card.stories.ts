36565d313e865f255059a7c51aeb23d7a018c14c
James Gao <392041299@qq.com>
2022-06-01 11:51:40 +0800

    chore: (wip) `Card`

--- src/runtime/components/card/card.stories.ts
+++ src/runtime/components/card/card.stories.ts
@@ -5,6 +5,7 @@ import NCard from './card.vue'
 const meta: Meta = {
   title: '设计系统/数据展示/卡片 Card',
   component: NCard,
+  decorators: [() => ({ template: '<div class="w-[300px]"><story /></div>' })],
 }
 
 export const Default: Story = (args) => ({
@@ -15,9 +16,13 @@ export const Default: Story = (args) => ({
   `,
 })
 Default.args = {
-  style: 'width: 300px',
-  title: '卡片标题',
   default: '卡片内容',
+}
+
+export const Title = Default.bind({})
+Title.args = {
+  ...Default.args,
+  title: '卡片标题',
 }
 
 export default meta
