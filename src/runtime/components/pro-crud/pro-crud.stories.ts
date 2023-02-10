f40108ac6c5ad04ad0f16ba4e0e37ae850922b16
gxmari007 <392041299@qq.com>
2023-02-10 23:43:37 +0800

    chore: (WIP) ProCrud

--- src/runtime/components/pro-crud/pro-crud.stories.ts
+++ src/runtime/components/pro-crud/pro-crud.stories.ts
@@ -1,5 +1,29 @@
-import type { Meta } from '@storybook/vue3'
+import type { Meta, Story } from '@storybook/vue3'
+
+import { data } from '../pro-table/pro-table.stories'
+import type { ProCrudColumn } from './types'
+import { ProCrud } from '.'
 
 export default {
-  title: '',
+  title: '高级组件/增删改查 Crud',
+  component: ProCrud,
 } as Meta
+
+const Template: Story = (args) => ({
+  components: { ProCrud },
+  setup: () => ({ args }),
+  template: '<pro-crud class="test" v-bind="args" />',
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  data,
+  columns: [
+    { prop: 'date', label: '日期' },
+    { prop: 'name', label: '姓名' },
+    { prop: 'state', label: '州' },
+    { prop: 'city', label: '城市' },
+    { prop: 'address', label: '地区' },
+  ] as ProCrudColumn[],
+  border: true,
+}
--- src/runtime/components/pro-crud/pro-crud.vue
+++ src/runtime/components/pro-crud/pro-crud.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <pro-table></pro-table>
+    <pro-table v-bind="$attrs" :data="data" :columns="columns"></pro-table>
   </div>
 </template>
 
@@ -8,10 +8,19 @@
 import type { PropType } from 'vue'
 
 import ProTable from '../pro-table/pro-table.vue'
+import type { ProTableData } from '../pro-table/types'
 import type { ProCrudColumn } from './types'
 
 defineProps({
   /** 表格列配置 */
   columns: { type: Array as PropType<ProCrudColumn[]>, default: () => [] },
+  /** 数据集 */
+  data: { type: Array as PropType<ProTableData>, default: () => [] },
 })
+</script>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+}
 </script>
